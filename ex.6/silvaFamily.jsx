9b5df0d6e1578c6fd7386f63277566c7d6661875
eudalio <tonnydallios@gmail.com>
2018-04-12 10:49:48 -0300

    Working with multiple components and the dependency between them

---
+++ ex.6/silvaFamily.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import Member from './member'
+
+export default props => (
+    <div>
+        <Member name='Eudálio' lastName='Silva' />
+        <Member name='Maria' lastName='Silva' />
+        <Member name='Bruno' lastName='Silva' />
+        <Member name='João' lastName='Silva' />
+    </div>
+)
