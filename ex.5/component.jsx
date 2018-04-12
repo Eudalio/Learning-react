55160e2b29c4ef28d3ffb897eaba8b6ac553266a
eudalio <tonnydallios@gmail.com>
2018-04-12 10:31:13 -0300

    passing more than one parameter and I understand import and export

---
+++ ex.5/component.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+export const PriComp = props => (
+    <h1> Primeiro Component! </h1>
+)
+
+const SegComp = props => (
+    <h1> Segundo Component! </h1>
+)
+
+export { SegComp }
+//npm run dev - Para executar o serviço
---
+++ ex.5/index.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+import { PriComp, SegComp } from './component'
+
+ReactDOM.render(
+    <div>
+        <PriComp />
+        <SegComp />
+    </div>
+    , document.getElementById('app'))
