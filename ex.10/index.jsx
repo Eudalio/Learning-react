93e8fafdf1ad27da95c28257557f8333525d4e42
eudalio <tonnydallios@gmail.com>
2018-04-22 23:42:29 -0300

    inserting text in the input field and reflecting on a label

---
+++ ex.10/index.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+import Field from './field'
+
+ReactDOM.render(
+    <Field initialValue='React é top' />
+    , document.getElementById('app'))
